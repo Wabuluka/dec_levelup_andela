9a5f7805c8284867ca3bfb840ca3d4bfc2cb20f3
Wabuluka <dwabuluka@gmail.com>
2019-01-13 12:30:26 +0300

    [chore #162694106] update

--- app/views/get_all_view.py
+++ app/views/get_all_view.py
@@ -1,11 +1,13 @@
 from . import *
 
+
 class GetAllCorruptionMap(MethodView):
     def get(self, id):
         if id is None:
             if len(red_flag_records) < 1:
                 return jsonify({"status":404,"message":"Resource does not exist" }),404
-            return jsonify({"status":200 ,"data": red_flag_records}),200
+            data =red_flag_records
+            return jsonify({"status":200 ,"data": data}),200
         else:
             for index in range(len(red_flag_records)):
                 if red_flag_records[index]["id"]== id: 
