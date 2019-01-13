fd638acbf6e5cd1a5d054cfd8929e4d8c344bb8a
Wabuluka <dwabuluka@gmail.com>
2019-01-13 12:33:31 +0300

    [chore #162694106] setup trial testing for general features

--- app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from .create_red_flag import RedFlagModel
-
-class AllValidators:
-    def validate_content_type(self, contentType):
-        if contentType == 'application/json':
-            return True
-        return jsonify({"status":400, "error":"The content type must be json"}), 400
