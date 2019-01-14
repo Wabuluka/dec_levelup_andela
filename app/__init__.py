7f40ec5fcad43dfa66076b281aa3d1bed09bac5b
Wabuluka <dwabuluka@gmail.com>
2019-01-14 13:23:40 +0300

    [chore #162694106] update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 from app.routes.get_all_route import route_url_all
 from app.routes.create_redflag_route import route_url_create
 from app.routes.create_user_route import route_url_create_new_user
 from app.routes.get_one_route import route_url_get_one_redflag
 from app.routes.delete_route import route_url_delete
-
-app = Flask(__name__)
-
