c9a065505208ef89914da81c1b01028867ebe360
Wabuluka <dwabuluka@gmail.com>
2019-01-14 08:36:14 +0300

    [chore #162694666] pytest cov

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,4 @@ app = Flask(__name__)
 
 from app.routes.get_all_route import route_url_all
 from app.routes.create_redflag_route import route_url_create
+# from app.routes.create_case_route import route_url_post
