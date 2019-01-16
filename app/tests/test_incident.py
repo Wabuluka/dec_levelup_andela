dbb9ea34e4b88401f16b9959f921ae798ce7a4a7
Wabuluka <dwabuluka@gmail.com>
2019-01-16 11:49:09 +0300

    [chore #162694106] testing

--- app/tests/test_incident.py
+++ app/tests/test_incident.py
@@ -4,7 +4,7 @@ import json
 class TestOneRedFlag(BaseTest):
 
     def test_get_one_redflag(self):
-        response = self.client.get('/api/v1/redflagrecord/1')
+        response = self.client.get('/api/v1/redflagrecords/1')
         data =  json.loads(response.data)
         self.assertEqual(data["status"],404)
 
@@ -12,3 +12,13 @@ class TestOneRedFlag(BaseTest):
         response = self.client.get('/api/v1/redflagrecords')
         data =  json.loads(response.data)
         self.assertEqual(data["status"],404)
+
+    def test_create_incident(self):
+        new_incident = {
+            "type": "intervention",
+            "location": "one",
+            "comment": "one",
+            "createdBy": "one",
+        }
+        response = self.client.post('/api/v1/redflagrecord', data = json.dumps(new_incident))
+        self.assertEqual(response.status_code, 201)
