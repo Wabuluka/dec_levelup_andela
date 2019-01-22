68d237fede3699cad3d13c2038a996200b99b057
Wabuluka <dwabuluka@gmail.com>
2019-01-22 10:25:35 +0300

    [ft #163348733] signup

--- app/routes/incident_routes.py
+++ app/routes/incident_routes.py
@@ -28,27 +28,27 @@ app.add_url_rule('/api/v1/red-flags/<int:id>/location',defaults = None,
                     view_func=route_url_edit_location, methods=['PATCH',])
 
 
-endpoints for the intervvention incident
-intervention_url = InterventionViewMap.as_view('create_intervention')
-app.add_url_rule('/api/v1/intervention',
-                    view_func=intervention_url, methods=['POST',])
 
-intervention_url = InterventionViewMap.as_view('get_all_interventions')
-app.add_url_rule('/api/v1/intervention', defaults = None, 
-                    view_func=intervention_url, methods=['GET',])
+# intervention_url = InterventionViewMap.as_view('create_intervention')
+# app.add_url_rule('/api/v1/intervention',
+#                     view_func=intervention_url, methods=['POST',])
 
-intervention_url = InterventionViewMap.as_view('get_one_intervention')
-app.add_url_rule('/api/v1/intervention/<int:id>',defaults = None, 
-                    view_func=intervention_url, methods=['GET',])
+# intervention_url = InterventionViewMap.as_view('get_all_interventions')
+# app.add_url_rule('/api/v1/intervention', defaults = None, 
+#                     view_func=intervention_url, methods=['GET',])
 
-intervention_url = InterventionViewMap.as_view('delete_one_intervention')
-app.add_url_rule('/api/v1/intervention/<int:id>',defaults = None, 
-                    view_func=intervention_url, methods=['DELETE',])
+# intervention_url = InterventionViewMap.as_view('get_one_intervention')
+# app.add_url_rule('/api/v1/intervention/<int:id>',defaults = None, 
+#                     view_func=intervention_url, methods=['GET',])
 
-intervention_url = InterventionViewMap.as_view('patch_comment_intervention')
-app.add_url_rule('/api/v1/intervention/<int:id>/comment',defaults = None, 
-                    view_func=intervention_url, methods=['PATCH',])
+# intervention_url = InterventionViewMap.as_view('delete_one_intervention')
+# app.add_url_rule('/api/v1/intervention/<int:id>',defaults = None, 
+#                     view_func=intervention_url, methods=['DELETE',])
 
-intervention_url = InterventionViewMap.as_view('patch_location_intervention')
-app.add_url_rule('/api/v1/intervention/<int:id>/location',defaults = None, 
-                    view_func=intervention_url, methods=['PATCH',])
+# intervention_url = InterventionViewMap.as_view('patch_comment_intervention')
+# app.add_url_rule('/api/v1/intervention/<int:id>/comment',defaults = None, 
+#                     view_func=intervention_url, methods=['PATCH',])
+
+# intervention_url = InterventionViewMap.as_view('patch_location_intervention')
+# app.add_url_rule('/api/v1/intervention/<int:id>/location',defaults = None, 
+#                     view_func=intervention_url, methods=['PATCH',])
--- app/views/user_views.py
+++ app/views/user_views.py
@@ -2,23 +2,22 @@ from . import *
 """
     creating a new user
 """
+usermodel = UserModel()
 class CreateNewUser(MethodView):
     
     def post(self):
+        if request.content_type != 'application/json':
+          return jsonify({ "status": "404", "message": "Change content_type to json" })
         data = request.get_json()
         
-        user = UserModel(
-          userid = len(users) + 1,
-          firstname = data['firstname'],
-          lastname = data['lastname'],
-          othername = data['othernames'],
-          email = data['email'],
-          phonenumber = data['phonenumber'],
-          username = data['username'],
-          registeredOn = data['registeredOn'],
-          isAdmin = data['isAdmin']
-        )
-        users.append(user.usr_dictionary())
-        return jsonify({"data": user.usr_dictionary()})
+        firstname = data['firstname'],
+        lastname = data['lastname'],
+        othernames = data['othernames'],
+        email = data['email'],
+        phonenumber = data['phonenumber'],
+        username = data['username'],
+        password = data['password']
 
-    
+        new_user = UserModel(firstname=firstname,lastname=lastname, othernames=othernames, email=email, phonenumber=phonenumber,username= username, password=password)
+        usermodel.create_user(new_user)
+        return jsonify({"status": 200, "message": "Created success"})
